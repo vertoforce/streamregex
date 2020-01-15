fdaff60ac4baba95d418101212565cd2668ff21d
Connor Lake <vertoforce@gmail.com>
2020-01-15 08:37:14 -0800

    Updated tests

--- streamregex_test.go
+++ streamregex_test.go
@@ -8,44 +8,48 @@ import (
 )
 
 func TestFindReader(t *testing.T) {
-	// Create string
-	data := `0123456789this is a stream    of data with lots of trailing information`
-	stream := strings.NewReader(data)
-
-	// Build regex
-	regex := regexp.MustCompile(`stream\s+of`)
-
-	// Find matches
-	matchedData := FindReader(context.Background(), regex, 40, stream)
-	matches := 0
-	for range matchedData {
-		matches++
-		// fmt.Println(string(match))
+	tests := []struct {
+		input       string
+		match       string
+		regex       *regexp.Regexp
+		maxMatchLen int
+		numMatchs   int
+	}{
+		{
+			`0123456789this is a stream    of data with lots of trailing information`,
+			`stream    of`,
+			regexp.MustCompile(`stream\s+of`),
+			40,
+			1,
+		},
+		{
+			`test test test test test test test test test`,
+			`test`,
+			regexp.MustCompile(`test`),
+			4,
+			9,
+		},
+		{
+			`cut off match`,
+			`f ma`,
+			regexp.MustCompile(`f ma`),
+			4,
+			1,
+		},
 	}
-	if matches != 1 {
-		t.Errorf("Did not find correct number of matches")
-	}
 
-	// Test for duplicates
-
-	// Create string
-	data = `test test test test test test test test test`
-	stream = strings.NewReader(data)
-
-	// Build regex
-	regex = regexp.MustCompile(`test`)
-
-	// Find matches
-	matchedData = FindReader(context.Background(), regex, 5, stream)
-	matches = 0
-	for match := range matchedData {
-		matches++
-		if match != "test" {
-			t.Errorf("Invalid match: %s", match)
+	for _, test := range tests {
+		// Find matches
+		matchedData := FindReader(context.Background(), test.regex, test.maxMatchLen, strings.NewReader(test.input))
+		matches := 0
+		for match := range matchedData {
+			matches++
+			if match != test.match {
+				t.Errorf("Invalid match: %s", match)
+			}
 		}
-		// fmt.Println(string(match))
-	}
-	if matches != 9 {
-		t.Errorf("Did not find correct number of matches")
+		if matches != test.numMatchs {
+			t.Errorf("Did not find correct number of matches")
+		}
 	}
 }
